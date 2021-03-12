45b806662b50368427eb0caf97da9844dce767b1
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2021-03-12 10:22:35 +0300

    Added script to create bin file in Linux

--- release.sh
+++ release.sh
@@ -7,22 +7,22 @@ else
     exit -1
 fi
 
-
 if [ -d venv ]; then rm -rf venv; fi;
 if [ -d dist ]; then rm -rf dist; fi;
 if [ -d build ]; then rm -rf build; fi;
 if [ -d release ]; then rm -rf release; fi;
 if [ -f release ]; then rm -f release; fi;
+
 python3 -m venv venv
 venv/bin/python -m pip install --upgrade pip
-venv/bin/python -m pip install -r requirements.txt
-
 venv/bin/python -m pip install pyinstaller==4.0
-venv/bin/pyinstaller --clean -F --add-binary "epcboot_gui/resources/linux/epcbootlib.so:." epcboot_gui/epcboot_gui.py
+
+venv/bin/pyinstaller --clean --onefile Crawl.py
+cp default.ini dist/
+cp readme.md dist/
 
 mv dist release
-cd release
+
 if [ -d build ]; then rm -rf build; fi;
 if [ -d dist ]; then rm -rf dist; fi;
-count='ls -1 *.spec 2>/dev/null | wc -l'
-if [[ $count != 0 ]]; then rm -rf *.spec; fi;
+if [ -f Crawl.spec ]; then rm Crawl.spec; fi;
